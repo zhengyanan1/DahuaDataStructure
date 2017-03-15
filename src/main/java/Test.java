b8a48143070a3b565206c80b2faae3e91cbd5ce5
zhengyanan <2821084381@qq.com>
2017-03-15 15:23:24 +0800

    first push

---
+++ src/main/java/Test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/3/15.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test.main():Hello World!!!");
+    }
+}
